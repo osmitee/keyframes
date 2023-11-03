e87bd7f0b0bf19f1982e212a33a1a8aac7c6c8e5
osmitee <obers735@newschool.edu>
2023-11-03 03:41:11 -0400

    cyberspace

---
+++ main/script.js
@@ -0,0 +1,52 @@
+
+let slideIndex = 1;
+
+// Wrap your code in a DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides(slideIndex);
+});
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides(slideIndex);
+
+    const keyDiv = document.getElementById("open");
+    const poemDiv = document.getElementById("myModal");
+    const overlay = document.getElementById("overlay");
+
+    keyDiv.addEventListener("click", function() {
+        poemDiv.style.display = "block";
+        overlay.style.display = "block"; // Show the overlay
+    });
+
+    document.addEventListener("click", function(event) {
+        if (event.target !== poemDiv && event.target !== keyDiv) {
+            poemDiv.style.display = "none";
+            overlay.style.display = "none"; // Hide the overlay
+        }
+    });
+});
